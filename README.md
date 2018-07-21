bf96416bbb3b7f5dc00094c7530d126cc54ca25f
_eternal_flame <eternal_flame-AD@protonmail.com>
2018-07-21 15:49:29 +0800

    zhan.com

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tampermonkey-scripts
-自用的tampermonkey脚本
+自用的tampermonkey脚本，持续更新
+
+- [去除小站zhan.com强制登陆界面，拒绝骚扰电话](https://raw.githubusercontent.com/eternal-flame-AD/tampermonkey-scripts/master/zhan.com.js)
---
+++ zhan.com.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Zhan.com nologin
+// @namespace    https://github.com/eternal-flame-AD/tampermonkey-scripts
+// @version      0.1
+// @description  去除小站登陆界面，拒绝骚扰电话
+// @author       eternal-flame-AD
+// @match        http://top.zhan.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    var target = document.querySelector("body");
+    var config = { attributes: false, childList: true, subtree: false };
+
+    var observer = null;
+
+    var remove = function () {
+        if (document.querySelector("div.bg_player_login")) {
+            console.log("Removing login background");
+            document.body.style = "overflow: auto !";
+            document.querySelector("div.regist_view_pop").remove();
+            document.querySelector("div.bg_player_login").remove();
+            observer.disconnect();
+        }
+    };
+
+    observer = new MutationObserver(remove);
+    observer.observe(target, config);
+
+})();
